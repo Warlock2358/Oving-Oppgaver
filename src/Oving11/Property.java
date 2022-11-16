72a594919e82d4d3d6432b9cba77ea18b891d162
Warlock2358 <63557128+Warlock2358@users.noreply.github.com>
2022-11-16 12:26:19 +0100

    Øving 11

--- src/Oving11/Property.java
+++ src/Oving11/Property.java
@@ -1,5 +1,138 @@
 package Oving11;
 
 public class Property {
+  private int municipalityNumber;
+  private final int lotNumber;
+  private final int sectionNumber;
+  private String municipalityName;
+  private String lotName;
+  private String nameOfOwner;
+  private final double area;
 
+  public Property(int municipalityNumber, int lotNumber, int sectionNumber, String municipalityName,
+      String lotName, String nameOfOwner, double area) {
+    testValues(municipalityNumber, lotNumber, sectionNumber, area, municipalityName, lotName, nameOfOwner); // Try catch in user interface
+    this.municipalityNumber = municipalityNumber;
+    this.lotNumber = lotNumber;
+    this.sectionNumber = sectionNumber;
+    this.municipalityName = municipalityName;
+    this.lotName = lotName;
+    this.nameOfOwner = nameOfOwner;
+    this.area = area;
+  }
+
+  public Property(Property property) {
+    this.municipalityNumber = property.municipalityNumber;
+    this.lotNumber = property.lotNumber;
+    this.sectionNumber = property.sectionNumber;
+    this.municipalityName = property.municipalityName;
+    this.lotName = property.lotName;
+    this.nameOfOwner = property.nameOfOwner;
+    this.area = property.area;
+  }
+
+  public void testValues(int municipalityNumber, int lotNumber, int sectionNumber, double area, String municipalityName, String lotName, String nameOfOwner) {
+    if (municipalityNumber < 101 || municipalityNumber > 5054) {
+      throw new IllegalArgumentException("Municipality number must be between 101 and 5054");
+    }
+    if (lotNumber < 0 || lotNumber > 9999) {
+      throw new IllegalArgumentException("Lot number must be between 0 and 999");
+    }
+    if (sectionNumber < 0 || sectionNumber > 9999) {
+      throw new IllegalArgumentException("Section number must be between 0 and 999");
+    }
+    if (area < 0) {
+      throw new IllegalArgumentException("Area must be positive");
+    }
+    if (municipalityName == null || municipalityName.isBlank()) {
+      throw new IllegalArgumentException("Municipality name must be a string");
+    }
+    if (lotName == null || lotName.isBlank()) {
+      throw new IllegalArgumentException("Lot name must be a string");
+    }
+    if (nameOfOwner == null || nameOfOwner.isBlank()) {
+      throw new IllegalArgumentException("Name of owner must be a string");
+    }
+  }
+
+  public int getMunicipalityNumber() {
+    return municipalityNumber;
+  }
+
+  public int getLotNumber() {
+    return lotNumber;
+  }
+
+  public int getSectionNumber() {
+    return sectionNumber;
+  }
+
+  public String getMunicipalityName() {
+    return municipalityName;
+  }
+
+  public String getLotName() {
+    return lotName;
+  }
+
+  public String getNameOfOwner() {
+    return nameOfOwner;
+  }
+
+  public double getArea() {
+    return area;
+  }
+
+  public void setMunicipalityNumber(int municipalityNumber) {
+    try {
+      testValues(municipalityNumber, lotNumber, sectionNumber, area, municipalityName, lotName, nameOfOwner);
+    } catch (IllegalArgumentException e) {
+      System.out.println(e.getMessage());
+    }
+    this.municipalityNumber = municipalityNumber;
+  }
+
+  public void setMunicipalityName(String municipalityName) {
+    try {
+      testValues(municipalityNumber, lotNumber, sectionNumber, area, municipalityName, lotName, nameOfOwner);
+    } catch (IllegalArgumentException e) {
+      System.out.println(e.getMessage());
+    }
+    this.municipalityName = municipalityName;
+  }
+
+  public void setLotName(String lotName) {
+    try{
+      testValues(municipalityNumber, lotNumber, sectionNumber, area, municipalityName, lotName, nameOfOwner);
+    } catch (IllegalArgumentException e) {
+      System.out.println(e.getMessage());
+    }
+    this.lotName = lotName;
+  }
+
+  public void setNameOfOwner(String nameOfOwner) {
+    try {
+      testValues(municipalityNumber, lotNumber, sectionNumber, area, municipalityName, lotName, nameOfOwner);
+    } catch (IllegalArgumentException e) {
+      System.out.println(e.getMessage());
+    }
+    this.nameOfOwner = nameOfOwner;
+  }
+
+  public String getPropertyId() {
+    return this.getMunicipalityNumber() + "-" + this.getLotNumber() + "/" + this.getSectionNumber();
+  }
+
+  public String toString() {
+    StringBuilder sb = new StringBuilder();
+
+    if (this.getLotName().equals("")) {
+      return sb.append("Municipality: ").append(this.getMunicipalityName()).append(", property id: ").append(this.getPropertyId())
+          .append(", area: ").append(this.getArea()).append("m^2, owner: ").append(this.getNameOfOwner()).toString();
+    } else {
+      return sb.append("Municipality: ").append(this.getMunicipalityName()).append(", property id: ").append(this.getPropertyId())
+          .append(", area: ").append(this.getArea()).append("m^2, owner: ").append(this.getNameOfOwner()).append(", lot name: ")
+          .append(this.getLotName()).toString();
+    }
+  }
 }
--- src/Oving11/PropertyRegister.java
+++ src/Oving11/PropertyRegister.java
@@ -1,5 +1,49 @@
 package Oving11;
 
+import java.util.ArrayList;
+
 public class PropertyRegister {
 
+  private ArrayList<Property> properties;
+
+  public PropertyRegister() {
+    properties = new ArrayList<>();
+  }
+
+  public ArrayList<Property> getProperties() {
+//    StringBuilder result = new StringBuilder();
+//    for (Property property : properties) {
+//      result.append(property.toString()).append("\n");
+//    }
+//    return result.toString();
+    ArrayList<Property> list = new ArrayList<>();
+    for (Property property : properties) {
+      Property propertiesCopy = new Property(property);
+      list.add(propertiesCopy);
+    }
+    return list;
+  }
+
+  public void registerProperty(int municipalityNumber, int lotNumber, int sectionNumber,
+      String municipalityName, String lotName, String nameOfOwner, double area) {
+    Property newProperty = new Property(municipalityNumber, lotNumber, sectionNumber,
+        municipalityName, lotName, nameOfOwner, area);
+  }
+
+  public void removeProperty(int municipalityNumber, int lotNumber, int sectionNumber) {
+    properties.removeIf(property -> property.getMunicipalityNumber() == municipalityNumber
+        && property.getLotNumber() == lotNumber
+        && property.getSectionNumber() == sectionNumber);
+  }
+
+  public ArrayList<Property> getPropertiesByMunicipality(String municipalityName) {
+    ArrayList<Property> list = new ArrayList<>();
+    for (Property property : properties) {
+      if (property.getMunicipalityName().equals(municipalityName)) {
+        Property propertiesCopy = new Property(property);
+        list.add(propertiesCopy);
+      }
+    }
+    return list;
+  }
 }
